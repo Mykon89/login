ea28a3e4803ec381b08e5767997169c0e36dcd3e
Mykon89 <helvis89@gmail.com>
2023-07-25 21:59:23 -0300

    reparação erro api

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,30 +1,31 @@
 import React from 'react';
 import api from '../api';
+// import axios from 'axios';
 import { useState, useEffect } from 'react';
 
 const Home = () => {
   const [livros, setLivros] = useState([]);
 
   useEffect(() => {
-    // Faz a chamada à API quando o componente é montado
-    api
-      .get('/livros')
-      .then(response => {
+    // Função para fazer a requisição à API
+    const getLivros = async () => {
+      try {
+        const response = await api.get('/livros'); // Substitua a URL pela rota da sua API
         setLivros(response.data);
-      })
-      .catch(error => {
-        console.error('Erro ao buscar livros:', error);
-      });
-  }, []);
+      } catch (error) {
+        console.error('Erro ao buscar dados da API:', error);
+      }
+    };
 
+    getLivros();
+  }, []);
   return (
     <div>
       <ul>
         {livros.map(livro => (
-          <li key={livro.id}>{livro.titulo}</li>
+          <li key={livro._id}>{livro.titulo}</li>
         ))}
       </ul>
-      <h1>oi</h1>
     </div>
   );
 };
--- server/index.js
+++ server/index.js
@@ -2,13 +2,6 @@ import app from './app.js';
 
 const port = process.env.PORT || 8000;
 
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  next();
-});
-
 app.listen(port, () => {
   console.log(`servidor escutando em http://localhost:${port}`);
 });
